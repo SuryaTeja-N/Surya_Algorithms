4edcc2630e19214e83d479358cab130e620eb9ba
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-10-01 12:45:56 -0500

    Time: 0 ms (100%), Space: 40.2 MB (99.12%) - LeetHub

---
+++ 0758-convert-binary-search-tree-to-sorted-doubly-linked-list/0758-convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+    Node first = null, last = null;
+    //we need to traverse the tree, we will do in-order DFS
+    private void recurse(Node curr){
+        if(curr == null) return;
+        //we need to go to leftmost (asusual inorder)
+        recurse(curr.left);
+        //2) see, here after we will get new value, so we need to do linkings with previous value
+        if (last != null){ //checking if previous is there or not
+           //link now
+           last.right = curr;
+           curr.left = last;
+        }else{
+            first = curr;
+        }
+        // apart from just looking each node, we need to do linkings, so lets keep track with last pointer
+        last = curr;
+        //we need to go to rightmost (asusual inorder)
+        recurse(curr.right);
+    }
+    public Node treeToDoublyList(Node root) {
+        if(root == null) return root;
+        recurse(root);
+        //close the DLL
+        last.right = first; first.left = last;
+        return first;
+    }
+}
