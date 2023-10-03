4f3639c47c5e105b5966da674c07eccc6ac55ecb
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-10-03 18:02:03 -0500

    Time: 1 ms (79.84%), Space: 41.2 MB (41.38%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,16 +1,22 @@
 class Solution {
-    HashMap<Character,String> map;
+
+    private HashMap<Character,String> map;
+    private List<String> ans;
+    private String digits;
+
     public List<String> letterCombinations(String digits) {
         if (digits.length()==0) return new ArrayList<>();
-        List<String> ans = new ArrayList<>();
+        ans = new ArrayList<>();
         //it is evident that we can use backtracking here, since constraint number is less
         //first, we need to assign numbers to the strings as in question
-        map = new HashMap<>();
-        map.put('2',"abc");map.put('3',"def");map.put('4',"ghi");map.put('5',"jkl");map.put('6',"mno");map.put('7',"pqrs");map.put('8',"tuv");map.put('9',"wxyz");
-        backtracking(ans,digits, new StringBuilder(),0);
+        map = new HashMap<>(); this.digits = digits;
+        map.put('2',"abc");map.put('3',"def");map.put('4',"ghi");map.put('5',"jkl");
+        map.put('6',"mno");map.put('7',"pqrs");map.put('8',"tuv");map.put('9',"wxyz");
+        backtracking(new StringBuilder(),0);
         return ans;
     }
-    private void backtracking(List<String> ans, String digits, StringBuilder curr,int index){
+    
+    private void backtracking(StringBuilder curr,int index){
        //base case
        if(curr.length() == digits.length()){
            ans.add(curr.toString()); return;
@@ -21,7 +27,7 @@ class Solution {
        //now start the loop
        for(char i : options.toCharArray()){
           curr.append(i); //add
-          backtracking(ans,digits,curr,index+1); //recurse and use
+          backtracking(curr,index+1); //recurse and use
           curr.deleteCharAt(curr.length()-1); //remove
        }
     }
