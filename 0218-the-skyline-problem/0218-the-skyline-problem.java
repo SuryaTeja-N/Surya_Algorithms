b307790c1e18071a197a5ab31b6550d4376a2baa
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-10-05 18:04:04 -0500

    Time: 118 ms (40.11%), Space: 46.2 MB (32.54%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.java
+++ 0218-the-skyline-problem/0218-the-skyline-problem.java
@@ -1,48 +1,39 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
         List<List<Integer>> ans = new ArrayList<>();
+        //previously we have used naive approvah, but now lets use sweep line algorithm
 
-        //first we need to get the points on x-axis without duplicates
-        Set<Integer> points = new TreeSet<>(); //we need to use TreeSet instead of hashset, beacuse we need to get in sorted order.
-        for (int[] point : buildings){
-            points.add(point[0]); points.add(point[1]);
-        }
-
-        //so, by now we have a fixed indexes in x plain in points set, now we need to find maximum heights of occupied building at that specific index
-
-        List<Integer> edges = new ArrayList<>(points); //this will convert that treeset to list (beacuse treeset do sorting but wont preserve the order)
-        
-        //so here edges values are real locations of buildings, so there there will so much difference in btw them, we need to map to indexs
-        Map<Integer,Integer> indexmap = new HashMap<>();
-        for(int i=0; i<edges.size(); ++i){
-            indexmap.put(edges.get(i), i);
-        }
-
-        //ok, lets think about heights too at each index now
-        int[] heights = new int[edges.size()];
-        //to find maximum height at each index, lets iterate over all buildings
+        //we maight have so many buildings with same positions, so lets remove duplicates and keep it sorted (to make them point to our indexs for easiness)
+        Set<Integer> set = new TreeSet<>(); //as hashset wont sort values
+        //fill the Set
         for(int[] building : buildings){
-            //for each building lets find the right and left index
-            int leftidx = indexmap.get(building[0]);
-            int rightidx = indexmap.get(building[1]);
-
-            //we will update the maximum height within range of [left, right)  {here we will exclude rightidx beacuse two buildings, will not share same point right!!!}
-            for(int i=leftidx; i<rightidx; ++i){
-               heights[i] = Math.max(heights[i], building[2]);
-            }
+            set.add(building[0]); set.add(building[1]);
         }
+        //but treeset is not good for accessing or traversing elements, so lets convert to List
+        List<Integer> positions = new ArrayList<>(set);
 
-        //as now we have got the max heights
-        //lets try to form the answer
-        for(int i=0; i<heights.length; ++i){
-            int currheight = heights[i];
-            int currpos = edges.get(i);
+        //so its time for using the sweep line algorithm
+        //basically we will take a imaginary line and traverse through left to right buildings on x-axis {and find where the height is chnaging and track the maxheight}
 
-            //Add all the positions where the height changes to 'answer'.
-            if(ans.isEmpty() || ans.get(ans.size()-1).get(1) != currheight){
-                ans.add(Arrays.asList(currpos,currheight));
+        //lets do that traversal
+        for(int pos : positions){ //at each position we need to update the answer
+            int max_height=0;
+            //move through buildings
+            for(int[] building : buildings){
+                int left = building[0],right = building[1];
+
+                //if the current building, intersect with the line, the update the max_height
+                if(left <= pos && right > pos){
+                    max_height = Math.max(max_height, building[2]);
+                }
             }
+            // we need to prepare the answer right
+           //we will add points {like (position,height) as asked in question} to answer only when the height is changed or if its the first entry
+           if (ans.isEmpty() || max_height != ans.get(ans.size()-1).get(1)){
+               ans.add(Arrays.asList(pos, max_height));
+           }
         }
         return ans;
     }
+    // worst case is: O(n^2), becauseno.of positions may be like 2n and no. of buildings are n. so in worst case its going to be O(2*n^2)
 }
