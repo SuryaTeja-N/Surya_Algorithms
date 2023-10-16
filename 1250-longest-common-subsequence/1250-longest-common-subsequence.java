5fcb5765d6419fc458f345ef5a86d7ee5d6f98c8
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-10-16 17:08:26 -0500

    Time: 49 ms (5.03%), Space: 48.1 MB (65.25%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    private String s1,s2;private int memo[][];
+    private int recurse(int p1, int p2){
+        if (p1 == s1.length() || p2 == s2.length()) return 0;
+       //check if we already claculated this
+       if(memo[p1][p2] != -1) return memo[p1][p2];
+       int choice1 = recurse(p1+1,p2); //exclude this value
+       //so second choice shoul be to include that  value
+       //if its there we need to look for its match in 2nd String
+       int firstseeins2 = s2.indexOf(s1.charAt(p1), p2); //we gave p2 parameter {so, it will start searching from p2 in second string}
+       int choice2 = 0;
+       if (firstseeins2 > -1){
+         choice2 = 1+ recurse(p1+1,firstseeins2+1);
+       }
+
+       //now compare and take best one
+       memo[p1][p2] = Math.max(choice1, choice2);
+
+       return memo[p1][p2];
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+        if(text1.length() ==0 || text2.length() ==0) return 0;
+        this.memo = new int[text1.length()+1][text2.length()+1];
+        for(int[] i : memo) Arrays.fill(i,-1);
+        this.s1 = text1;this.s2 = text2;
+        return recurse(0,0);
+    }
+}
