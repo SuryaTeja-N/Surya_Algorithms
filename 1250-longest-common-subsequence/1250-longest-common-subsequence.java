3560ec13c7f726db63e4a3dbf065757af1a1a038
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-10-17 16:37:26 -0500

    Time: 32 ms (9.62%), Space: 48.2 MB (52.67%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -19,10 +19,19 @@ class Solution {
 
        return memo[p1][p2];
     }
+    private int lcs(int p1, int p2){
+        if(p1 == s1.length() || p2 == s2.length()) return 0;
+        if(memo[p1][p2] != -1) return memo[p1][p2];
+        if(s1.charAt(p1) == s2.charAt(p2)){
+            memo[p1][p2] = 1+lcs(p1+1,p2+1);
+        } 
+         else memo[p1][p2] = Math.max(lcs(p1,1+p2),lcs(1+p1,p2));
+        return memo[p1][p2];
+    }
     public int longestCommonSubsequence(String text1, String text2) {
         memo = new int[text1.length()+1][text2.length()+1];
         for(int[] i : memo) Arrays.fill(i,-1);
         s1 = text1;s2 = text2;
-        return recurse(0,0);
+        return lcs(0,0);
     }
 }
