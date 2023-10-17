5440c0c4522fee2eb5935a4ca861ded613b6c4d0
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-10-17 18:47:07 -0500

    Time: 22 ms (54.34%), Space: 53.4 MB (6.53%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -22,9 +22,7 @@ class Solution {
     private int lcs(int p1, int p2){
         if(p1 == s1.length() || p2 == s2.length()) return 0;
         if(memo[p1][p2] != -1) return memo[p1][p2];
-        if(s1.charAt(p1) == s2.charAt(p2)){
-            memo[p1][p2] = 1+lcs(p1+1,p2+1);
-        } 
+        if(s1.charAt(p1) == s2.charAt(p2)) memo[p1][p2] = 1+lcs(p1+1,p2+1);
          else memo[p1][p2] = Math.max(lcs(p1,1+p2),lcs(1+p1,p2));
         return memo[p1][p2];
     }
@@ -32,6 +30,18 @@ class Solution {
         memo = new int[text1.length()+1][text2.length()+1];
         for(int[] i : memo) Arrays.fill(i,-1);
         s1 = text1;s2 = text2;
-        return lcs(0,0);
+        //return lcs(0,0);
+        //lets deal with bottom up approach,
+        int dp[][] = new int[text1.length()+1][text2.length()+1];
+        for(int row= text1.length()-1; row>=0; row--){
+            for(int col=text2.length()-1; col >=0; col--){
+                if(text1.charAt(row) == text2.charAt(col)){
+                  dp[row][col] = 1+ dp[row+1][col+1];
+                }else{
+                  dp[row][col] = Math.max(dp[row][col+1],dp[row+1][col]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
