82150dd59211b31a7e67bfcc6550c16641ee4c7e
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-11-04 12:38:21 -0500

    Time: 60 ms (80.07%), Space: 17.6 MB (93.86%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,15 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        maxprofit = 0
-
-        for i in range(1,len(prices)):
-           
-           if (prices[i] > prices[i-1]):
-
-               maxprofit += prices[i] - prices[i-1];
-
-        return maxprofit
-            
-
         
+        # we can solve with finite state meachine approach
+
+        profit = 0 # here we dont have any empty state similar to best time to buy and sell stock 3 (As we are always having unlimited transactions, there will be no instance for completed transactions, contiuesly we will taking transactions) 
+
+        # and also you can see here we are not considering the sell point, because from buy point (very less price) to we contiuesly checking the high sell point to get great profit, so final sell point itself has the big profit
+
+        buyone = -inf
+
+        for price in prices:
+            
+            buyone = max (buyone , profit-price)
+
+            profit = max(profit , buyone+price)
+
+        return profit
