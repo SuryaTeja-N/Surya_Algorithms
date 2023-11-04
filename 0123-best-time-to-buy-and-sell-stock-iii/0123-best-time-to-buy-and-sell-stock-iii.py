8ebb39130de2b11bea482a2435400d719afb32ba
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-11-04 15:54:37 -0500

    Time: 833 ms (82.66%), Space: 30.5 MB (83.99%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,13 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        # this approach is called "finite state meachine"
-        empty = 0
+        # this approach is called "finite state meachine" (applicable for all best time to buy and sell stock problems)
+
+        curr_profit = 0  #empty state
 
         onebuy = onesell = twobuy = twosell = -inf
 
         for price in prices:
 
-            onebuy , onesell , twobuy , twosell = max( empty - price , onebuy) , max (onebuy + price , onesell) , max(onesell - price, twobuy) , max(twobuy + price , twosell)
+            onebuy = max (onebuy,  curr_profit - price)
 
-        return max( empty , onebuy , onesell , twobuy, twosell)
+            onesell = max (onesell, onebuy + price)
+
+            twobuy = max (twobuy, onesell - price)
+
+            twosell = max (twosell, twobuy + price)
+
+        return twosell
