818fbf85d41724615661b38284588e9375842f77
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-11-05 17:31:33 -0600

    Time: 39 ms (95.52%), Space: 16.5 MB (82.65%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -2,16 +2,15 @@ class Solution:
 
     def maxProfit(self, prices: List[int]) -> int:
 
-        sold, held, reset = float('-inf'), float('-inf'), 0
+        # here " held " state represent both holding and buying states
+
+        sold, held, reset = -inf, -inf, 0
 
         for price in prices:
-            # Alternative: the calculation is done in parallel.
-            # Therefore no need to keep temporary variables
-            sold, held, reset = held + price, max(held, reset-price), max(reset, sold)
-
-            # pre_sold = sold
-            # sold = held + price
-            # held = max(held, reset - price)
-            # reset = max(reset, pre_sold)
+            
+            pre_sold = sold
+            sold = held + price
+            held = max(held, reset - price)
+            reset = max(reset, pre_sold)
 
         return max(sold, reset)
