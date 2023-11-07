2369430ab1acc488992790a3c650beaee933612c
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-11-07 12:36:29 -0600

    Time: 746 ms (20.41%), Space: 32.2 MB (62.27%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.py
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.py
@@ -1,18 +1,17 @@
 class Solution:
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
-        leni = len(dist); monster = []
+        leni = len(dist); monster_heap = []
 
         for i in range(leni):
-            monster.append(dist[i] / speed[i])
+            monster_heap.append(dist[i] / speed[i])
 
-        monster.sort()
+        heapq.heapify(monster_heap)
         
         kill=0
 
-        for i in range(len(monster)):
-            if monster[i] <= i:
-                break
-            kill +=1
+        while monster_heap:
+            if heapq.heappop(monster_heap) <= kill: break
+            kill += 1
         
         return kill
 
