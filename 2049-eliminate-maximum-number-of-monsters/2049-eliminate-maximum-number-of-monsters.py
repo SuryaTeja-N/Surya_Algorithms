0eb37fde3d7918d4f3f6533b97dfdae9daa0b3fe
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-11-07 15:35:13 -0600

    Time: 690 ms (76.74%), Space: 31.8 MB (97.59%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.py
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.py
@@ -1,19 +1,19 @@
 class Solution:
+    # O(n) Solution
+
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
         n=len(dist)
         monsters_t=[0]*(n+1)
 
         for i in range(n):
-            # the following is ceil function (inbuit ceil in python is slow)
-            #t=(dist[i]+speed[i]-1)//speed[i] 
             t = math.ceil(dist[i] / speed[i])
-            if t>n: 
+            if t>n:   # due to this, the t value is always going to be <= n
                 t=n
-            monsters_t[t]+=1
+            monsters_t[t]+=1  # this list will helpful to tell at which time, how many monsters are coming
         
         shot=1
         monsters=0
-        while shot<=n:
+        while shot<=n:  # beacause we have n chances (n shots) to kill all n monsters
             monsters+=monsters_t[shot-1]
             if shot<=monsters: 
                 break
@@ -21,4 +21,14 @@ class Solution:
         shot-=1
         return shot
         
-        
+
+# Explanation : 
+
+'''
+1) why checking t>n ?
+
+sol : see.. for every minute, despite of distace of monster, we can kill it, so, if there are 5 monsters, if we have 5 minutes of time, we can kill all of them (each one at each minute).
+
+so, in short if time is greater than "n" minutes, there is no need to check, all monsters are going to be dead. so t=n
+
+'''
