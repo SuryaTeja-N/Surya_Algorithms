a1d4b3cb07de92cddd3b264d1a0cc28b7a7848d9
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-11-07 19:51:27 -0600

    Time: 48 ms (66.23%), Space: 16.3 MB (98.87%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,14 +4,18 @@ class Solution:
 
         # here " held " state represent both holding and buying states
 
-        sold, buy_held, reset = -inf, -inf, 0
+        sold, buy, reset = -inf, -inf, 0
 
         for price in prices:
 
-            buy_held = max(buy_held, reset - price)
+            # buy = max(buy, reset - price) # what will be there before buy? , its reset -> so buy from it
 
-            reset = max(reset, sold)
+            # reset = max(reset, sold)  # what will be there before reset, its sold -> so compare with it
             
-            sold = max(sold, buy_held + price)
+            # sold = max(sold, buy + price) # what will be there before sold? , its buy , so add price to it
 
-        return sold
+            #buy,sold,reset = max(buy, reset - price) , max(reset, sold) , max(sold, buy + price)
+
+            reset , buy , sold = max(reset, sold) , max(buy, reset - price) , max(sold, buy + price)
+
+        return max(sold , reset, buy)
