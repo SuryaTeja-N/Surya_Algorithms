7f8df394f3c8ae53aaa5b01621fb10d7eebc346d
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-11-10 11:29:41 -0600

    Time: 995 ms (49.43%), Space: 37.2 MB (14.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,18 +2,17 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
         @functools.cache
-        def recurse(self, amount : int):
+        def recurse(amount : int):
             # lets handle base case when amount becomes zero
             if(amount == 0): return 0
             if (amount<0): return -1
             res = float('inf')
             for i in coins:
                 #what if i include the coin
-                include = recurse(self, amount-i)
+                include = recurse(amount-i)
                 if include == -1 : continue
                 res = min(res, include+1)
             if res == float('inf') : return -1
             return res
 
-        if amount ==0 : return 0
-        return recurse(self, amount)
+        return recurse(amount)
