553a1e00339903834f29afef87b45986f0b02fd1
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-11-10 17:33:22 -0600

    Time: 771 ms (98.39%), Space: 27.2 MB (65.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -14,25 +14,24 @@ class Solution:
 
         # # if you observe carefully, its similar to maximum subarray problem, and we can use kadane's algorithm
         
-        # mini = float('inf')
-        # max_profit = 0
-        # for price in prices:
-        #     if (mini > price):
-        #         mini = price
-        #     elif (max_profit - mini < price): # if current price is reducing the profit, neglect it, if it is increasing, then update the profit.
-        #         max_profit = price-mini
-        # return max_profit
-
+        mini = float('inf')
+        max_profit = 0
+        for price in prices:
+            if (mini > price):
+                mini = price
+            elif (max_profit < price - mini): # if current price is reducing the profit, neglect it, if it is increasing, then update the profit.
+                max_profit = price-mini
+        return max_profit
 
       # as similar to other " best time to buy and sell stock problems", we can use finite state meachine approach
 
-        empty = 0 #curr_profit
-        onebuy = onesell = -inf
+        # empty = 0 #curr_profit
+        # onebuy = onesell = -inf
 
-        for price in prices:
+        # for price in prices:
 
-            onebuy = max(empty - price, onebuy)
-            onesell = max(onebuy + price, onesell)
+        #     onebuy = max(empty - price, onebuy)
+        #     onesell = max(onebuy + price, onesell)
 
-        return onesell
+        # return onesell
             
