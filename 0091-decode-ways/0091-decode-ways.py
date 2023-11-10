54e8d6555696bed75a0e516d8a40cdaad9c189ef
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-11-10 15:05:03 -0600

    Time: 42 ms (44%), Space: 16.4 MB (38.88%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -7,12 +7,13 @@ class Solution:
         @functools.cache
         def recurse(curr_idx) -> int:
             #lets handle the base cases
-            if curr_idx == len(s): return 1
-            if s[curr_idx] == '0' : return 0
-            if curr_idx == len(s)-1 : return 1
-
-            if int(s[curr_idx : curr_idx + 2]) <= 26 : 
+            if curr_idx == len(s): return 1 # in counting dp, after completion, we need to count as 1
+            if s[curr_idx] == '0' : return 0 # if any string started with '0', answer is 0 {0 ways}
+            if curr_idx == len(s)-1 : return 1 # if we left with only one character and its not zero, then is one way
+            
+            if int(s[curr_idx : curr_idx + 2]) <= 26 :
                 return recurse(curr_idx+1) + recurse(curr_idx+2)
+
             return recurse(curr_idx+1)
 
         return recurse(0)
