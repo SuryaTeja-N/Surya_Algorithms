bd06e0dec1b342a0017aba96b132d44511aef401
Surya Teja <49818539+SuryaTeja-N@users.noreply.github.com>
2023-11-10 14:53:40 -0600

    Time: 36 ms (78.25%), Space: 16.6 MB (8.73%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        # you can see that, here we can split the given string s into different ways, but the catch is that it should not be prefixed with "0" {since there will be no alphabet starting with that}.
+
+        # so, to split them and tarck we can use counting dp here,
+        @functools.cache
+        def recurse(s, curr_idx) -> int:
+            #lets handle the base cases
+            if curr_idx == len(s): return 1
+            if s[curr_idx] == '0' : return 0
+            if curr_idx == len(s)-1 : return 1
+
+            if int(s[curr_idx : curr_idx + 2]) <= 26 : 
+                return recurse(s, curr_idx+1) + recurse(s,curr_idx+2)
+            return recurse(s,curr_idx+1)
+
+        return recurse(s,0)
